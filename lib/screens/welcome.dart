34aed038c2b9f17605a93bdb9a017b0a79be69d4
CodeRebirth <sixpathssenin@gmail.com>
2021-04-18 16:00:20 +0530

    animation added

--- lib/screens/welcome.dart
+++ lib/screens/welcome.dart
@@ -7,17 +7,33 @@ class Welcome extends StatefulWidget {
 }
 
 class _WelcomeState extends State<Welcome> with TickerProviderStateMixin{
+  AnimationController animationController;
+  Animation<double> animate;
+@override
+  void initState() {
+    animationController = new AnimationController(vsync: this,duration: Duration(milliseconds:500));
+    animate = Tween<double>(begin: 0,end:1).animate(animationController)..addListener(() {
+      setState(() {
+        
+      });
+    });
+    animationController.forward();
+    super.initState();
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-    floatingActionButton:  Container(height:70,width:70,child: FloatingActionButton(backgroundColor: Colors.amber,child:Icon(Icons.arrow_right,size:50),onPressed: (){
+    floatingActionButton:  Container(height:70,width:70,child: FloatingActionButton(backgroundColor: Colors.black,child:Icon(Icons.arrow_right,size:50),onPressed: (){
       Navigator.of(context).pushNamed("homepage");
     },)),
     floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     body:Stack(
       children: [
-        Container(
-          color: Colors.amber[300],
+        Transform.scale(
+          scale: animate.value,
+            child: Container(
+            color: Colors.amber[300],
+          ),
         ),
          ClipPath(
         clipper: Clipper(),
@@ -25,7 +41,7 @@ class _WelcomeState extends State<Welcome> with TickerProviderStateMixin{
               height: MediaQuery.of(context).size.height,
               width: MediaQuery.of(context).size.width,
               decoration: BoxDecoration(gradient: LinearGradient(begin: Alignment.bottomCenter,end: Alignment.topCenter,
-              colors: [Colors.blueAccent[700],Colors.red])),
+              colors: [Colors.amber[700],Colors.red])),
               child:Column(children: [
               ],)
             ),
