d1ab1b1176bb7ead42f8c54a560926d284a04b9f
CodeRebirth <sixpathssenin@gmail.com>
2021-04-19 16:37:35 +0530

    Changed homescreen code

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -4,61 +4,42 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body:Stack(
-            alignment: Alignment.center,
-            children: [
-              Container(
-                height: MediaQuery.of(context).size.height,
-                width: MediaQuery.of(context).size.width,
-                color:Colors.amber
-              ),
-              Column(
+        body:Container(
+          height:MediaQuery.of(context).size.height,
+          width: double.infinity,
+          decoration: BoxDecoration(
+            color: Colors.yellow[200]
+          ),
+          child:Column(children: [
+            SizedBox(height:MediaQuery.of(context).size.height*0.05),
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  SizedBox(height:MediaQuery.of(context).size.height*0.04,),
-                  Align(
-                    alignment: Alignment.topCenter,
-                      child: Padding(
-                        padding: const EdgeInsets.only(left:10.0,right:10.0),
-                        child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                        InkWell(onTap: (){},child: Icon(Icons.home,size: 35)),
-                        InkWell(onTap: (){},child: Icon(Icons.settings,size: 35,)),
-                        ],
-                    ),
-                      ),
-                  ),
+                  Icon(Icons.home,size:30),
+                  Icon(Icons.settings,size:30),
                 ],
               ),
+            ),
+            SizedBox(height:MediaQuery.of(context).size.height*0.05),
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.7,
+              width: MediaQuery.of(context).size.width * 0.9,
+              child: PageView(children: [
               Container(
-                    height:500,
-                    width: 400,
-                    color: Colors.transparent,
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(100),
-                       child: PageView(
-                       children: [
-                         Container(
-                            color: Colors.white,
-                            child: Center(child:Text("Billing")),
-                         ),
-                         Container(
-                            color: Colors.blue,
-                            child: Center(child:Text("Inventory")),
-                         ),
-                         Container(
-                            color: Colors.orange,
-                            child: Center(child:Text("History")),
-                         ),
-                       ],
-                      ),
-                    ),
-                  ),
-                  Positioned(
-                    bottom: 100,
-                    child: Text("Scroll",style: Theme.of(context).textTheme.headline1,))
-            ], 
-          )
+                color: Colors.amber,
+              ),
+              Container(
+                color: Colors.blue,
+              ),
+              Container(
+                color: Colors.white
+              ),
+              ],),
+            )
+          ],)
+        )
     );
   }
 }
