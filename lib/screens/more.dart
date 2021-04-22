329f89f793ed796476372bdef2538e9e2644b4ca
CodeRebirth <sixpathssenin@gmail.com>
2021-04-22 14:20:41 +0530

    more completed

--- lib/screens/more.dart
+++ lib/screens/more.dart
@@ -1,15 +1,43 @@
 import 'package:flutter/material.dart';
 import '../widgets/emptyClipper.dart';
 
-class More extends StatelessWidget {
+class More extends StatefulWidget {
+  @override
+  _MoreState createState() => _MoreState();
+}
+
+class _MoreState extends State<More> with TickerProviderStateMixin{
+  AnimationController scaleController;
+  Animation<double> animation;
+
+@override
+  void initState() {
+    scaleController = new AnimationController(vsync: this,duration: Duration(milliseconds: 400));
+    animation = Tween<double>(begin: 0,end:1).animate(CurvedAnimation(parent:scaleController,curve: Curves.easeOut,))..addListener(() {setState(() {
+      
+    });});
+    scaleController.forward();
+    super.initState();
+  }
   @override
   Widget build(BuildContext context) {
-    return ClipPath(
-        clipper: MoreClipper(),
-        child: Container(
-        color: Colors.amber,
-        child:Center(child:Text("Devloped by Leonard",style:TextStyle(color: Colors.black,fontWeight: FontWeight.bold,fontSize:30,fontFamily:"Philoshoper"),) ,)
-      ),
+    return ScaleTransition(
+      alignment: Alignment.bottomLeft,
+          scale:animation,
+          child: ClipPath(
+          clipper: MoreClipper(),
+          child: Container(
+          color: Colors.amber,
+          child: Center(
+            child:Text("Devloped by Leonard",
+            style:TextStyle(color: Colors.black,fontWeight: FontWeight.bold,fontSize:30,fontFamily:"Philoshoper"),) 
+            ,))
+        ),
     );
+  }
+  @override
+  void dispose() {
+    scaleController.dispose();
+    super.dispose();
   }
 }
